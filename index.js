c8d4aeb12e627cad5f1af890333bc8eeed4a90a7
Michael Toth <tofudrummer@gmail.com>
2013-09-06 23:11:17 -0400

    Hello World up and running?

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require("http");
+//var twitterAPI = require("node-twitterAPI");
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-type": "text-plain"});
+    response.end("Hello world!");
+});
+
+/*var twitter = new twitterAPI({
+    consumerKey: "A key",
+    consumerSecret: "A secret",
+    callback: "URL?"
+});
+*/
+
+
+server.listen(9000);
