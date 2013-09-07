3203ff8b05a5c56860f7158efaf291cb032dfcbe
Michael Toth <tofudrummer@gmail.com>
2013-09-06 23:38:56 -0400

    Changed app name

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require("express");
+var twitterAPI = require("node-twitter-api");
+
 var app = express();
+var twitter = new twitterAPI({
+    consumerKey: "WK64NFjcPJGCb8lyNovw",
+    consumerSecret: "uT70twf20pZc6UqdSkFflB2DWiIY6D9GwvY0pmIk",
+    callback: "http://glacial-reef-2380.herokuapp.com/"
+    });
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
